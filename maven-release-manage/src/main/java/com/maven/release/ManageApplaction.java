2c6791e6c8471f4e4adf3ff6eab1178432eb5653
lenovo-PC\lenovo <544303626@qq.com>
2017-12-06 11:28:11 +0800

    no message

---
+++ maven-release-manage/src/main/java/com/maven/release/ManageApplaction.java
@@ -0,0 +1,17 @@
+package com.maven.release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ManageApplaction 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ManageApplaction.class, args);
+    }
+}
---
+++ maven-release/maven-release-service/src/main/java/com/maven/release/service/TestService.java
@@ -0,0 +1,16 @@
+package com.maven.release.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Hello world!
+ *
+ */
+@Service
+public class TestService 
+{
+    public String showName(){
+    	
+    	return "asdasdasdasad";
+    }
+}
