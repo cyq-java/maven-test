9f8bde8a3a65f1406500df9d3f72e1745add1b6b
lenovo-PC\lenovo <544303626@qq.com>
2017-12-12 14:38:04 +0800

    no message

---
+++ release-maven-a/src/main/java/org/release/maven/a/App.java
@@ -0,0 +1,13 @@
+package org.release.maven.a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
