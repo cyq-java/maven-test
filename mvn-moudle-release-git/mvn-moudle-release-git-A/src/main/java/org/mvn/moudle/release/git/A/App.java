faa138712c4f74e3737f4b0cdf1dd2cd7fa38c16
lenovo-PC\lenovo <544303626@qq.com>
2017-12-12 14:33:36 +0800

    no message

---
+++ mvn-moudle-release-git/mvn-moudle-release-git-A/src/main/java/org/mvn/moudle/release/git/A/App.java
@@ -0,0 +1,13 @@
+package org.mvn.moudle.release.git.A;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
